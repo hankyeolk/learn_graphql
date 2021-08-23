168d5d183cad2b657efe6d8938a0be3b7167ae87
hankyeolk <2247han@gmail.com>
2021-08-23 14:56:19 +0900

    210823 add reference code

--- GraphQL/index.js
+++ GraphQL/index.js
@@ -2,23 +2,51 @@ const { ApolloServer, gql } = require('apollo-server');
 const {
   ApolloServerPluginLandingPageGraphQLPlayground
 } = require('apollo-server-core');
+const {
+  getUserData,
+  getUserByCohort,
+  addUser,
+  deleteUserById
+} = require('./util-model');
 
 const typeDefs = gql`
   type Query {
-    // todo
+    getUserById(uid: Int): [User]
+    getUserByCohort(cohort_num: Int): [User]
   }
   type Mutation {
-    // todo
+    addUser(input: AddUserInput): Boolean
+    deleteUserById(uid: Int): Boolean
   }
-  // todo
+  type User {
+    uid: Int!
+    name: String
+    cohort_num: Int
+  }
+  input AddUserInput {
+    name: String!
+    cohort_num: Int
+  }
 `;
 
 const resolvers = {
+  User: {
+    name: (parent) => {
+      console.log(parent.name);
+      return parent.name.toUpperCase();
+    }
+  },
   Query: {
-    // todo
+    getUserById: (parent, args, context, info) => getUserData(args.uid),
+    getUserByCohort: (parent, args, context, info) =>
+      getUserByCohort(args.cohort_num)
   },
   Mutation: {
-    // todo
+    addUser: (parent, args, context, info) => {
+      const { name, cohort_num } = args.input;
+      return addUser(name, cohort_num);
+    },
+    deleteUserById: (parent, args, context, info) => deleteUserById(args.uid)
   }
 };
 
@@ -27,7 +55,6 @@ const server = new ApolloServer({
   resolvers,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground]
 });
-
 server.listen().then(({ url }) => {
   console.log(`graphql server is running on ${url}`);
 });
